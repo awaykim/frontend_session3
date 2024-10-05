4779d066b226948c493a3f4cb69549e947a4af86
awaykim <wonj010101@gmail.com>
2024-10-05 19:00:55 +0900

    수정

--- app/page.js
+++ app/page.js
@@ -11,20 +11,20 @@ export default function Home() {
           <Image
             src={netflix}
             alt="Netflix Logo"
-            width={150}
+            width={140}
             height={40}
           ></Image>
-          <button class="bg-red-600 px-3 py-2 rounded-md text-white hover:bg-red-700 transition">로그인</button>
+          <button class="bg-red-600 mt-2 mr-2 w-[72px] h-[30px] rounded-md text-sm text-white ">로그인</button>
         </div>
         <div class="text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <h1 class="text-3xl font-semibold">영화, 시리즈 등을 <br></br>무제한으로</h1>
-            <p class="text-lg mt-4">5,500원으로 시작하세요. 맴버십은 언제든 해지 가능.</p>
-            <p class="text-sm mt-2">시청할 준비가 되셨나요? 맴버십을 등록하거나 재시작하려면 이메일 주소를 입력하세요</p>
+            <p class="text-base mt-10">5,500원으로 시작하세요. 맴버십은 언제든 해지 가능.</p>
+            <p class="text-base mt-10">시청할 준비가 되셨나요? 맴버십을 등록하거나 재시작하려면 이메일 주소를 입력하세요</p>
             <input 
               type="email" 
               placeholder="이메일 주소"
-              class="p-2 me-2 mt-3 rounded-l bg-gray-800 text-gray-300 placeholder-gray-500"></input>
-            <button class="bg-red-600 px-6 py-2 rounded-md text-white " >시작하기</button>
+              class="p-2 me-2 mt-3 rounded-md bg-gray-800 text-gray-300 placeholder-gray-500 outline outline-1 outline-gray-300"></input>
+            <button class="text-base text-xl bg-red-600 px-6 py-2 rounded-md text-white">시작하기  &gt;</button>
         </div>
       </div>
   </>
